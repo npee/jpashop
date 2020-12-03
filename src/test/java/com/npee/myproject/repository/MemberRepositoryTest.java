847af4aa8b82631ed2b4b89373a5e8b3de2561cb
npee <jmc4664@gmail.com>
2020-12-04 00:51:25 +0900

    영속성 컨텍스트 내부의 엔티티가 같은지 확인하는 테스트

--- src/test/java/com/npee/myproject/repository/MemberRepositoryTest.java
+++ src/test/java/com/npee/myproject/repository/MemberRepositoryTest.java
@@ -20,7 +20,6 @@ public class MemberRepositoryTest {
 
     @Test
     @Transactional
-    @Rollback(false)
     public void testMember() throws Exception {
         // given
         Member member = new Member();
@@ -33,6 +32,7 @@ public class MemberRepositoryTest {
         // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 
 }
