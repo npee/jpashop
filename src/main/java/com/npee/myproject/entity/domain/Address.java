69e9d7612face81ac9ed2cc38d1fed04a6cb1e5b
npee <jmc4664@gmail.com>
2020-12-06 00:05:23 +0900

    값 타입에 대한 설정 추가

--- src/main/java/com/npee/myproject/entity/domain/Address.java
+++ src/main/java/com/npee/myproject/entity/domain/Address.java
@@ -1,14 +1,20 @@
 package com.npee.myproject.entity.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
-@Embeddable
+@Embeddable // 값 타입
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // reflection 이나 proxy 등의 기술을 사용하기 위해서 기본 생성자가 필요하다.
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
 }
+
