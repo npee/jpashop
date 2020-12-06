5750cc23c98b1e98f70fb9673ebe267c23669b03
npee <jmc4664@gmail.com>
2020-12-07 01:03:27 +0900

    리파지토리 클래스 작성

---
+++ src/main/java/com/npee/myproject/entity/domain/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package com.npee.myproject.entity.domain.repository;
+
+import com.npee.myproject.entity.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private void save(Member member) {
+        em.persist(member);
+    }
+
+    private Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
