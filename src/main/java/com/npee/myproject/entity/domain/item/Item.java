89b56efc1a04a4f8f7326788d4852b6c9c0df0c5
npee <jmc4664@gmail.com>
2020-12-11 22:12:59 +0900

    Custom exception 생성, Item 남은 수량 제어하는 비즈니스 로직 생성

--- src/main/java/com/npee/myproject/entity/domain/item/Item.java
+++ src/main/java/com/npee/myproject/entity/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.npee.myproject.entity.domain.item;
 
 import com.npee.myproject.entity.domain.Category;
+import com.npee.myproject.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,26 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // === 비즈니스 로직 ===
+
+    /**
+     * stock 증가
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * @param quantity
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity -= quantity;
+    }
 }
