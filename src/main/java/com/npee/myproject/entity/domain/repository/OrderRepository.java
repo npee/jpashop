731319c441e15b7f3dbf9ee985c46c61db6e3ad3
npee <jmc4664@gmail.com>
2020-12-13 20:44:04 +0900

    Order 리파지토리 생성

---
+++ src/main/java/com/npee/myproject/entity/domain/repository/OrderRepository.java
@@ -0,0 +1,23 @@
+package com.npee.myproject.entity.domain.repository;
+
+import com.npee.myproject.entity.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+}
