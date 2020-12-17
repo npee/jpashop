e0668d6643a94b660128e2e51f2236a57ad750dc
npee <jmc4664@gmail.com>
2020-12-17 23:49:55 +0900

    상품주문 주문취소 테스트

--- src/test/java/com/npee/myproject/service/OrderServiceTest.java
+++ src/test/java/com/npee/myproject/service/OrderServiceTest.java
@@ -72,9 +72,21 @@ public class OrderServiceTest {
     @Test
     public void 주문취소() throws Exception {
         // given
-        
+        Member member = createMember();
+        Book book = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 2;
+
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
         // when
+        orderService.cancelOrder(orderId);
+
         // then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals("주문 취소시 상태는 CANCEL 이다.", OrderStatus.CANCEL, getOrder.getStatus());
+        assertEquals("주문이 취소된 상품은 그만큼 재고가 늘어나야 한다.", 10, book.getStockQuantity());
     }
 
     private Book createBook(String name, int price, int stockQuantity) {
