cfbaad23edb028b1ca7b5fd6bd356396bdb76604
npee <jmc4664@gmail.com>
2020-12-22 00:00:31 +0900

    회원 조회 Controller, Form 작성

--- src/main/java/com/npee/myproject/controller/MemberController.java
+++ src/main/java/com/npee/myproject/controller/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -41,5 +42,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
