facb817582cb168248480c03515df35d4d35e63a
npee <jmc4664@gmail.com>
2020-12-27 19:59:01 +0900

    상품 주문 - 주문 Post Controller

--- src/main/java/com/npee/myproject/controller/OrderController.java
+++ src/main/java/com/npee/myproject/controller/OrderController.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -31,6 +33,15 @@ public class OrderController {
 
         return "order/orderForm";
     }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
 
 
 }
