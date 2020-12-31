f67e2b5cba153cbf1ce1903ba4cd1c668e10da9d
npee <jmc4664@gmail.com>
2020-12-31 23:37:12 +0900

    api 제작 - 회원가입 api 추가

---
+++ src/main/java/com/npee/myproject/api/MemberApiController.java
@@ -0,0 +1,31 @@
+package com.npee.myproject.api;
+
+import com.npee.myproject.entity.domain.Member;
+import com.npee.myproject.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class CreateMemberResponse {
+        private Long id;
+    }
+}
