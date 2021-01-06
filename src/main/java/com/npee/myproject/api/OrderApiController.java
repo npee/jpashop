88f1d5e77c4e60a1d78847371c29b40132febf5d
npee <jmc4664@gmail.com>
2021-01-06 20:55:07 +0900

    api 제작 - 주문 컬렉션 조회 (worst case)

---
+++ src/main/java/com/npee/myproject/api/OrderApiController.java
@@ -0,0 +1,30 @@
+package com.npee.myproject.api;
+
+import com.npee.myproject.entity.domain.Order;
+import com.npee.myproject.entity.domain.OrderItem;
+import com.npee.myproject.entity.domain.repository.OrderRepository;
+import com.npee.myproject.entity.domain.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        // 강제 초기화
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+}
