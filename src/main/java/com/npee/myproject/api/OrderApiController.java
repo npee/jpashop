84ddb9b343e36c167b9e35eb008cdf01a5fbc3eb
npee <jmc4664@gmail.com>
2021-01-07 23:30:48 +0900

    api 제작 - 주문 컬렉션 조회 2 (worst case): DTO와 엔티티의 관계를 완전히 끊어야 한다.

--- src/main/java/com/npee/myproject/api/OrderApiController.java
+++ src/main/java/com/npee/myproject/api/OrderApiController.java
@@ -1,14 +1,20 @@
 package com.npee.myproject.api;
 
+import com.npee.myproject.entity.domain.Address;
 import com.npee.myproject.entity.domain.Order;
 import com.npee.myproject.entity.domain.OrderItem;
+import com.npee.myproject.entity.domain.OrderStatus;
 import com.npee.myproject.entity.domain.repository.OrderRepository;
 import com.npee.myproject.entity.domain.repository.OrderSearch;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +32,33 @@ public class OrderApiController {
             orderItems.forEach(o -> o.getItem().getName());
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<OrderDto> collect = orders.stream().map(o -> new OrderDto(o)).collect(Collectors.toList());
+        return collect;
+    }
+
+    @Getter
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItem> orderItems;
+
+        public OrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+            order.getOrderItems().forEach(o -> o.getItem().getName()); // 강제 초기화
+            this.orderItems = order.getOrderItems();
+        }
     }
 }
