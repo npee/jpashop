51316d9e014b66c7d646cedc4b3426de5e3e31e1
npee <jmc4664@gmail.com>
2021-01-11 17:55:49 +0900

    api 페이징 개선 - 주문 컬렉션 조회 5 - v3.1: 페이징 쿼리 테스트

--- src/main/java/com/npee/myproject/api/OrderApiController.java
+++ src/main/java/com/npee/myproject/api/OrderApiController.java
@@ -9,6 +9,7 @@ import com.npee.myproject.entity.domain.repository.OrderSearch;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -48,8 +49,9 @@ public class OrderApiController {
     }
 
     @GetMapping("/api/v3.1/orders")
-    public List<OrderDto> ordersV3_page() {
-        List<Order> orders = orderRepository.findAllWithMemberDelivery(); // 페이징에 영향 주지 않음
+    public List<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                        @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
         List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).collect(Collectors.toList());
         return result;
     }
